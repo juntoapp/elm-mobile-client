a2e1ecce5d3222605d5e85575bd9c985d112ea4a
Johannes Klumpe <klumpe@live.de>
2017-09-27 00:12:27 +0200

    added dashboard calendar

--- src/Dashboard.elm
+++ src/Dashboard.elm
@@ -2,40 +2,33 @@ module Dashboard exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (class, value, href)
-import Date exposing (Date)
 import Msgs
 import Models
+import DashboardCalendar
 
 
 view : Models.Model -> Html Msgs.Msg
 view model =
     div [ class "dashboard" ]
-        [ calendar model ]
+        [ header model ]
+
+
+header : Models.Model -> Html Msgs.Msg
+header model =
+    div
+        [ class "dashboard__header" ]
+        [ calendar model
+        ]
 
 
 calendar : Models.Model -> Html Msgs.Msg
 calendar model =
-    div
-        [ class "dashboard__header" ]
-        [ div [ class "calendar__weekdays--title" ]
+    div []
+        ([ div
+            [ class "calendar__weekdays--title" ]
             ([ "M", "T", "W", "T", "F", "S", "S" ]
                 |> List.map (\x -> div [ class "calendar__weekday--title" ] [ text x ])
             )
-        , div
-            [ class "calendar__weekdays" ]
-            [ calendarDays model ]
-        ]
-
-
-calendarDays : Models.Model -> Html Msgs.Msg
-calendarDays model =
-    let
-        today =
-            case model.routeLoaded of
-                Just date ->
-                    Date.day date
-
-                _ ->
-                    1
-    in
-        (text << toString) today
+         ]
+            ++ (DashboardCalendar.calendarDays model)
+        )
